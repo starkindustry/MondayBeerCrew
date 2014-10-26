7402086254fa069b57015bd914a129989c2b96dc
parham <parham@dhcp-206-87-197-214.ubcsecure.wireless.ubc.ca>
2014-10-26 09:34:48 -0700

    some refactoring and clean up. will add packages next.

--- src/Fuser.java
+++ src/Fuser.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Fuser {
-
+	private  List<Klass> classList = new ArrayList<Klass>();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		XMLParser parser = new XMLParser("Result3.xml");
+		parser.printClasses();
 	}
 
 }
--- src/XMLParser.java
+++ src/XMLParser.java
@@ -11,13 +11,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class XMLParser {
-	private static List<Klass> classList = new ArrayList<Klass>();
+	private  List<Klass> classList = new ArrayList<Klass>();
 	// code stolen from http://www.mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/  
-	public static void main(String argv[]) {
+	public XMLParser(String filePath) {
 
 		try {
 
-			File fXmlFile = new File("Result3.xml");
+			File fXmlFile = new File(filePath);
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(fXmlFile);
@@ -37,10 +37,7 @@ public class XMLParser {
 					//class name
 					String className = classElement.getElementsByTagName("name").item(0).getTextContent();
 
-					NodeList outboundChildNodes = classElement.getElementsByTagName("outbound");			
-					System.out.println("\nCurrent Class : " + className);
-				
-					System.out.println("Outbound dependencies: ");
+					NodeList outboundChildNodes = classElement.getElementsByTagName("outbound");				
 					
 					//outbound dependents
 					ArrayList<String> outbounds = new ArrayList<String>();
@@ -50,17 +47,39 @@ public class XMLParser {
 								&& !dep.contains(".ui.") 		// Do not include UI-related dependencies
 								&& !dep.contains(".event.")) 	// Do not include event related dependencies
 							outbounds.add(dep);
-					}
-					for (String s : outbounds)
-						System.out.println(s);
-					
+					}					
 					Klass theClass = new Klass(className, outbounds);
 					classList.add(theClass);
 				}
 			}
+			
+			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void printClasses() {
+		System.out.println("Total classes: " + classList.size());
+		System.out.println("--------------------------------");
+		for (int i = 0; i < classList.size(); i++){
+			System.out.println((i+1) + ". " + classList.get(i).getName());
+			System.out.println("Dependencies: ");
+			List<String> deps = classList.get(i).getOutDependencies();
+			for (String s : deps)
+				System.out.println(s);
+			System.out.println("--------------------------------");
+		}	
+	}
+	
+	public List<Klass> getClassList(){
+		return this.classList;
+	}
+	
+//	public static void main(String argv[]){
+//		XMLParser parser = new XMLParser("Result.xml");
+//		parser.printClasses();
+//	}
 
 }
