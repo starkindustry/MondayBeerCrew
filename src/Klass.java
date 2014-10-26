6adcc7e611f8ee7f532e3d0ac7bb86a135306614
parham <parham@dhcp-206-87-197-214.ubcsecure.wireless.ubc.ca>
2014-10-25 17:50:36 -0700

    Parser generates list of classes with their outbound dependencies

---
+++ src/Klass.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+
+public class Klass {
+	private String name;
+//	private String packageName;
+//	private List<String> inDependencies;
+	private List<String> outDependencies;
+	
+	public Klass(String name, List<String> out){
+		this.name = name;
+//		this.packageName = packageName;
+//		this.inDependencies = in;
+		this.outDependencies = out;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+//	public String getPachageName(){
+//		return this.packageName;
+//	}
+	
+//	public List<String> getInDependencies(){
+//		return this.inDependencies;
+//	}
+	
+	public List<String> getOutDependencies(){
+		return this.outDependencies;
+	}
+
+}
---
+++ src/XMLParser.java
@@ -0,0 +1,69 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XMLParser {
+	private static List<Klass> classList = new ArrayList<Klass>();
+	// code stolen from http://www.mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/  
+	public static void main(String argv[]) {
+
+		try {
+
+			File fXmlFile = new File("/Users/parham/Desktop/Result.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			//optional, but recommended
+			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+			doc.getDocumentElement().normalize();
+
+			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+			NodeList classes = doc.getElementsByTagName("class");
+
+			System.out.println("----------------------------");
+
+			for (int i = 0; i < classes.getLength(); i++) {
+
+				Node currClass = classes.item(i);
+
+				if (currClass.getNodeType() == Node.ELEMENT_NODE) {
+					Element classElement = (Element) currClass;
+					
+					//class name
+					String className = classElement.getElementsByTagName("name").item(0).getTextContent();
+
+					NodeList outboundChildNodes = classElement.getElementsByTagName("outbound");			
+					System.out.println("\nCurrent Class : " + className);
+				
+					System.out.println("Outbound dependencies: ");
+					
+					//outbound dependents
+					ArrayList<String> outbounds = new ArrayList<String>();
+					for (int j = 0; j < outboundChildNodes.getLength(); j++){
+						String dep = classElement.getElementsByTagName("outbound").item(j).getTextContent();
+						if (!outbounds.contains(dep))
+							outbounds.add(dep);
+					}
+					for (String s : outbounds)
+						System.out.println(s);
+					
+					Klass theClass = new Klass(className, outbounds);
+					classList.add(theClass);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
