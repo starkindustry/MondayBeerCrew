e1c7bed16030596c0f378bd09ed2a41a48ccd8d4
Allan Fong <allan.s.fong@gmail.com>
2014-10-26 17:12:28 -0700

    created hashmaps to encapsulate LOC and complexity in fuser

--- src/Fuser.java
+++ src/Fuser.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 public class Fuser {
@@ -14,6 +15,15 @@ public class Fuser {
 //		XMLParser parser2 = new XMLParser("Result XMLs/DepFinderVntfulResults.xml");
 //		System.out.println("Code base: Vntful");
 //		parser2.printClasses();
+		
+		CSVParserSM csvReader = new CSVParserSM();
+		System.out.println("Class size by LOC:");
+		Map<String, String> linesOfCodeMap = csvReader.mapSmCSV("Result XMLs/SMVntfulDetails.csv", 4);
+		
+		System.out.println("--------------------------------");
+		System.out.println("Class complexity:");
+		Map<String, String> complexityMap = csvReader.mapSmCSV("Result XMLs/SMVntfulDetails.csv", 14);
+		
 		
 		XMLParser parser = new XMLParser("Result XMLs/testXmls/test1.xml");
 		System.out.println("Code base: No classes");
