86e3ccf6801ba78135fc66506b6b6ad1f17c71b5
Allan Fong <allan.s.fong@gmail.com>
2014-10-26 17:03:32 -0700

    added csvParser, updated csv files

---
+++ src/CSVParserSM.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class CSVParserSM {
+
+	/*Add the filename and metric from each java file into a HashMap
+	 * Taken from http://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/ */
+	public Map<String,String> mapSmCSV(String filepath, int metricIndex) {
+		Map<String, String> metric = new HashMap<String, String>();
+		
+		BufferedReader bReader = null;
+		String line = "";
+		String csvSeparator = ";"; //set separator in SM to semi-colon
+		String filename = null;
+		try {
+			bReader = new BufferedReader(new FileReader(filepath));
+			bReader.readLine();
+			while ((line = bReader.readLine()) != null) {
+				String [] jFile = line.split(csvSeparator);
+				filename = jFile[3].substring(jFile[3].lastIndexOf("\\") + 1);
+				metric.put(filename, jFile[metricIndex]);
+				
+			}
+			for (Map.Entry<String, String> entry : metric.entrySet()) {
+				System.out.println(entry.getKey() + ", " + entry.getValue());
+			};
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (bReader != null) {
+				try {
+					bReader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return metric;	
+	}
+}
--- src/Klass.java
+++ src/Klass.java
@@ -6,12 +6,21 @@ public class Klass {
 	private String packageName;
 //	private List<String> inDependencies;
 	private List<String> outDependencies;
+	private String linesOfCode;
+	private String complexityScore;
+	
 	
 	public Klass(String name, String packageName, List<String> out){
+		
 		this.name = name;
 		this.packageName = packageName;
 //		this.inDependencies = in;
 		this.outDependencies = out;
+		/** 
+		 * TODO: implement loc and complexity score to klass
+		this.linesOfcode = loc;
+		this.complexityScore = complexScore;
+		*/
 	}
 	
 	public String getName(){
@@ -29,5 +38,12 @@ public class Klass {
 	public List<String> getOutDependencies(){
 		return this.outDependencies;
 	}
-
+	
+	public String getLinesOfCode() {
+		return this.linesOfCode;
+	}
+	
+	public String getComplexityScore() {
+		return this.complexityScore;
+	}
 }
