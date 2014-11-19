c1a3c0cce15e976de31b894dc3ed34726386b042
parham <parham@192.168.1.79>
2014-11-18 19:58:23 -0800

    Added test code base for unit tests for the fuser

---
+++ CodeBases/Test/java files/PizzaMaker.java
@@ -0,0 +1,13 @@
+
+public class PizzaMaker {
+
+	public static void main(String[] args) {
+		
+		Pizza basicPizza = new TomatoSauce(new Mozzarella(new PlainPizza()));
+		
+		System.out.println("Ingredients: " + basicPizza.getDescription());
+		
+		System.out.println("Cost: " + basicPizza.getCost());
+		
+	}
+}
---
+++ CodeBases/Test/java files/TomatoSauce.java
@@ -0,0 +1,25 @@
+
+public class TomatoSauce extends ToppingDecorator{
+
+	public TomatoSauce(Pizza newPizza) {
+		super(newPizza);
+		
+		System.out.println("Adding Sause");
+		
+
+	}
+
+	
+	
+	public String getDescription() {
+	
+		return tempPizza.getDescription() + ", Tomato Sauce";
+	}
+
+
+	public double getCost() {
+
+		return tempPizza.getCost() + 0.50;
+	}
+
+}
