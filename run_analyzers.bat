da3cb32522cd8c3a766c4cbb472b3c4c687bbec9
Drew Wei <drewngui@gmail.com>
2014-11-19 17:23:31 -0800

    modified config file to use relative paths.  Added JAVA_HOME and DEPFINDER_PATH variables for first time setups.

--- run_analyzers.bat
+++ run_analyzers.bat
@@ -1,12 +1,15 @@
 for /f "delims=" %%x in (config.txt) do (set "%%x")
 
-start DependencyExtractor -class-filter -xml -out %DF_OUTPUT% -filter-excludes /^java\./ %CODEBASE_PATH%
-start %SM_PATH% /C %SM_CONFIG_PATH%
+set JAVA_HOME=%JAVA_HOME%
+set PATH=%DEPFINDER_PATH%;%PATH%
+
+start DependencyExtractor -class-filter -xml -out %~dp0\%DF_OUTPUT% -filter-excludes /^java\./ %~dp0\%CODEBASE_PATH%
+start %SM_PATH% /C %~dp0\%SM_CONFIG_PATH%
 sleep 2
 start javaw -jar %PROJECT_JAR% %JAR_FST_ARG% %JAR_SND_ARG%
 echo %JAR_FST_ARG%
 echo %JAR_SND_ARG%
-start "Chrome" chrome --new-window --allow-file-access-from-files %VISUALIZER%
+start "Chrome" chrome --new-window --allow-file-access-from-files %~dp0\%VISUALIZER%
 
 
 
